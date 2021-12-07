bf89dcfead96aaa6131e1820547658ec549658b2
Avvi Singh <singh.avvi98@gmail.com>
2021-12-08 01:00:02 +1100

    global constant arrays added and englishToMorse() rudimentary working

--- js/index.js
+++ js/index.js
@@ -1,5 +1,92 @@
 //main functions:
 //englishToMorse()
 //morseToEnglish()
+const englishCharSet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    ",",
+    "?",
+];
 
-let englishToMorse = (str) => {};
+const morseCharSet = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+    "-----",
+    ".----",
+    "..---",
+    "...--",
+    "....-",
+    ".....",
+    "-....",
+    "--...",
+    "---..",
+    "----.",
+    "--..--",
+    "..--..",
+];
+
+let englishToMorse = (str) => {
+    let charArr = str.split("");
+    let returnArr = charArr.map(
+        (char) => morseCharSet[englishCharSet.indexOf(char)]
+    );
+    return returnArr.join(" ");
+};
---
+++ js/index.test.js
@@ -0,0 +1,4 @@
+//Test for enlgish->morse
+//function should return correct alphabets and numbers i.e (englishToMorse('a') should return '.-')
+//function should return a space for a space
+//function should return error message if character entered is not in charSet
