4d81acaf004b956525a5cb43b69cca9554eadf2b
Avvi Singh <singh.avvi98@gmail.com>
2021-12-08 17:50:41 +1100

    data folder init and englishToMorse working with sentences

--- js/index.js
+++ js/index.js
@@ -1,92 +1,17 @@
-//main functions:
-//englishToMorse()
-//morseToEnglish()
-const englishCharSet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    ",",
-    "?",
-];
+import englishCharSet from "./data/englishCharSet.js";
+import morseCharSet from "./data/morseCharSet.js";
 
-const morseCharSet = [
-    ".-",
-    "-...",
-    "-.-.",
-    "-..",
-    ".",
-    "..-.",
-    "--.",
-    "....",
-    "..",
-    ".---",
-    "-.-",
-    ".-..",
-    "--",
-    "-.",
-    "---",
-    ".--.",
-    "--.-",
-    ".-.",
-    "...",
-    "-",
-    "..-",
-    "...-",
-    ".--",
-    "-..-",
-    "-.--",
-    "--..",
-    "-----",
-    ".----",
-    "..---",
-    "...--",
-    "....-",
-    ".....",
-    "-....",
-    "--...",
-    "---..",
-    "----.",
-    "--..--",
-    "..--..",
-];
-
-let englishToMorse = (str) => {
-    let charArr = str.split("");
-    let returnArr = charArr.map(
-        (char) => morseCharSet[englishCharSet.indexOf(char)]
-    );
+let englishToMorse = (str, engArr, mcArr) => {
+    let charArr = str.toLowerCase().split("");
+    let returnArr = charArr.map((char) => {
+        if (char === " ") {
+            return " / ";
+        }
+        return mcArr[engArr.indexOf(char)];
+    });
     return returnArr.join(" ");
 };
+
+console.log(
+    englishToMorse("Hello, my name is Avvi", englishCharSet, morseCharSet)
+);
